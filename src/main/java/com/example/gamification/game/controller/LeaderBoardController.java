20590c03fed22d9a98f73dd7ad5a294694d6c15a
Mohamed Amine Oueslati <amine.oueslati@aderivatives.com>
2024-09-18 14:01:19 +0100

    Add spring cloud for service discovery with consul and spring actuator for health check

--- src/main/java/com/example/gamification/game/controller/LeaderBoardController.java
+++ src/main/java/com/example/gamification/game/controller/LeaderBoardController.java
@@ -3,6 +3,7 @@ package com.example.gamification.game.controller;
 import com.example.gamification.game.domain.LeaderBoardRow;
 import com.example.gamification.game.service.LeaderBoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/leaders")
 @RequiredArgsConstructor
+@Slf4j
 public class LeaderBoardController {
 
     private final LeaderBoardService leaderBoardService;
 
     @GetMapping
     public List<LeaderBoardRow> getLeaderBoard() {
+        log.info("Retrieving leaderboard");
         return leaderBoardService.getCurrentLeaderBoard();
     }
 }
